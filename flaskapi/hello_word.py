c4cb96401f019f428ef97132f94f2875f67360ad
ricardo <ricardoabcd3@gmail.com>
2022-08-01 18:11:29 -0500

    a

--- flaskapi/hello_word.py
+++ flaskapi/hello_word.py
@@ -1,6 +1,25 @@
+#python
+from typing import Optional
+#pydantic
+from pydantic import BaseModel
+#fast api
 from fastapi import FastAPI
-i=788976
+from fastapi import Body
+
 app = FastAPI()
+#models 
+class Person(BaseModel):
+	first_name :str
+	last_name :str
+	age :int 
+	hair_color: Optional[str]=None
+	is_married: Optional[bool]=None
+
 @app.get('/')
 def home():
 	return (1)
+#request and response body
+@app.post("/person/new")
+def create_person(person: Person=Body(...)):
+	
+	return person	
